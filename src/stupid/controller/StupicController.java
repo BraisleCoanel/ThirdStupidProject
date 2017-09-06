734303d94eddf69599ff6650c4631cc52ea1b651
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2017-09-06 13:24:53 -0600

    Initial commit

---
+++ src/stupid/controller/StupicController.java
@@ -0,0 +1,9 @@
+package stupid.controller;
+
+public class StupicController
+{
+	public void start()
+	{
+		System.out.println("I like to mess with cards");
+	}
+}
---
+++ src/stupid/controller/StupidRunner.java
@@ -0,0 +1,10 @@
+package stupid.controller;
+
+public class StupidRunner
+{
+	public static void main(String [] args)
+	{
+		StupicController app = new StupicController();
+		app.start();
+	}
+}

a663b82c98317cb921888e115d364767a60fa529
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2017-09-28 12:42:50 -0600

    Added scanners.

--- src/stupid/controller/StupicController.java
+++ src/stupid/controller/StupicController.java
@@ -1,9 +1,27 @@
 package stupid.controller;
 
+import java.util.Scanner;
+
+
 public class StupicController
 {
 	public void start()
 	{
 		System.out.println("I like to mess with cards");
+		
+		testScanner();
+	}
+	
+	private void testScanner()
+	{
+		Scanner firstScanner; 
+		firstScanner = new Scanner(System.in);
+		System.out.println("What is your favorite food?");
+		String answer = firstScanner.nextLine();
+		System.out.println("Oh cool, you like "+ answer + ", that's pretty neato my dude!");
+		System.out.println("What's your favorite color?");
+		String answer2 = firstScanner.nextLine();
+		System.out.println("Wow, "+ answer2 + ", is an ugly color, liking that is the same as sinning!");
+		
 	}
 }
